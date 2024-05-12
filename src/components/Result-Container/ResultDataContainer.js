4f31efab24a1de53cdb122b51efcb00fcae70817
Mayur Badole <mayur.badole@ubiqc.com>
2024-05-12 21:07:21 +0530

    all set inside a filter section

--- src/components/Result-Container/ResultDataContainer.js
+++ src/components/Result-Container/ResultDataContainer.js
@@ -1,13 +1,18 @@
+import { zeviIcon } from "../../utils/svgImages";
 import Filters from "../Filters/Filters";
 import ProductsContainer from "../Products-Container/ProductsContainer";
 import "./ResultDataContainer.css";
 
 const ResultDataContainer = () => {
   return (
-    <div className="body-container">
-      <Filters />
-      <ProductsContainer />
-    </div>
+    <>
+      <img src={zeviIcon} alt="zevi logo " className="zevi-icon"/>
+      
+      <div className="body-container">
+        <Filters />
+        <ProductsContainer />
+      </div>
+    </>
   );
 };
 
